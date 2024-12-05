4aaedc7e128cd5ec4f0b944251d85b42a293590f
Vitalii Mudryi <vmudrij0508@gmail.com>
2024-12-05 22:07:02 +0200

    lesson 10

---
+++ lesson-10/src/app.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+import routes from './routes/index.js';
+
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+
+app.use('/api', routes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+export default app;
---
+++ lesson-10/src/middlewares/auth.js
@@ -0,0 +1,38 @@
+import createHttpError from 'http-errors';
+
+import { Session } from '../models/session.js';
+import { User } from '../models/user.js';
+
+export async function auth(req, res, next) {
+  const { authorization } = req.headers;
+
+  if (typeof authorization !== 'string') {
+    return next(createHttpError(401, 'Please provide access token'));
+  }
+
+  const [bearer, accessToken] = authorization.split(' ', 2);
+
+  if (bearer !== 'Bearer' || typeof accessToken !== 'string') {
+    return next(createHttpError(401, 'Please provide access token'));
+  }
+
+  const session = await Session.findOne({ accessToken });
+
+  if (session === null) {
+    return next(createHttpError(401, 'Session not found'));
+  }
+
+  if (session.accessTokenValidUntil < new Date()) {
+    return next(createHttpError(401, 'Access token is expired'));
+  }
+
+  const user = await User.findById(session.userId);
+
+  if (user === null) {
+    return next(createHttpError(401, 'User not found'));
+  }
+
+  req.user = { id: user._id, name: user.name };
+
+  next();
+}
